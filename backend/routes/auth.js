1847de65f296b5eebacc4fd4fdfffa4c4c4fdc29
Aman Choudhary <caman0987@gmail.com>
2024-04-28 18:25:00 +0530

    installing react-router-dom concurrently

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -9,9 +9,9 @@ const jwt_Secret = 'helloIamGoodboy';
 
 //Route 1 : To create a user Using POST: 6000/api/auth/createUser
 router.post("/createUser",[
-      body('name', 'name must be 3 characters').isLength({ min: 3}),
-      body('email', 'email must be valid').isEmail(),
-      body('password' , 'Password must contain 5 characters').isLength({ min: 5 }),
+  body('name', 'name must be 3 characters').isLength({ min: 3}),
+  body('email', 'email must be valid').isEmail(),
+  body('password' , 'Password must contain 5 characters').isLength({ min: 5 }),
   ],async(req, res) => {
     const errors = validationResult(req);
 
@@ -46,47 +46,46 @@ router.post("/createUser",[
     catch(error){
         console.error(error.message);
         res.status(500).send("Bad request");
-    }
-      
+    }      
 });
 
 // Route 2 : to login using POST: 6000/api/auth/loginUser
 
 router.post("/loginUser",[
-  body('email', 'email must be valid').isEmail(),
-  body('password' , 'Password must contain 5 characters').isLength({ min: 5 }),
-],async(req, res) => {
-  const errors = validationResult(req);
+    body('email', 'email must be valid').isEmail(),
+    body('password' , 'Password must contain 5 characters').isLength({ min: 5 }),
+    ],async(req, res) => {
+    const errors = validationResult(req);
 
-//checks If error is found, then return error with bad message request.
-  if (!errors.isEmpty()) {
-  return res.status(400).json({ errors: errors.array() });
-  }
+    //checks If error is found, then return error with bad message request.
+    if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+    }
 
-  const {email , password} = req.body;
-  try {
-    let user = await User.findOne({email});
-    //check if the usre exist or not
-    if(!user){
-      return res.status(400).json({error : "Email doesn't exist..."});
-    }
-    //check if the compared password is right or not.
-    const comparePassword = await bcrypt.compare(password, user.password);
-    if(!comparePassword){
-      return res.status(400).json({error : "Incorrect password.."});
-    }
-    const data ={
-      user : {
-        id : user.id
+    const {email , password} = req.body;
+    try {
+      let user = await User.findOne({email});
+      //check if the usre exist or not
+      if(!user){
+        return res.status(400).json({error : "Email doesn't exist..."});
       }
-    }
-    const authToken = jwt.sign(data ,jwt_Secret);
-    res.json(authToken);
-    
-  } catch (error) {
+      //check if the compared password is right or not.
+      const comparePassword = await bcrypt.compare(password, user.password);
+      if(!comparePassword){
+        return res.status(400).json({error : "Incorrect password.."});
+      }
+      const data ={
+        user : {
+          id : user.id
+        }
+      }
+      const authToken = jwt.sign(data ,jwt_Secret);
+      res.json(authToken);
+      
+    } catch (error) {
       Console.error(error.message);
       res.status(500).send("Bad request");
-  }
+    }
 });
 
 //Route 3: get the loggedin user by using POST: 6000/api/auth/getUser
