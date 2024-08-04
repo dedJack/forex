6848ccee18b7262827c5e464ddffea0a88c83920
dedJack <caman0987@gmail.com>
2024-08-04 19:03:07 +0530

    ...

--- src/components/Benefits.js
+++ src/components/Benefits.js
@@ -74,8 +74,31 @@ const Benifits = () => {
             <div className="col" key={benefits.a} >
               <div className="benefitsCard card text-bg-dark mb-5 bg-body-dark rounded" >
                 <div className="card-body" >
-                  <h5 className="card-title align-middle" style={{ textAlign: 'center' }} >
+                  <h5 className="card-title  align-middle" style={{ textAlign: 'center' }} >
                     <Link to={benefits.links} className="links">{benefits.a}</Link> </h5>
+                  {/* ---------------------------------------------------------------- */}
+                  <div className='modelbtn text-center'>
+                    <button type="button" className="modelbtn1 btn btn-primary " data-bs-toggle="modal" data-bs-target="#exampleModal">
+                      Read more
+                    </button>
+                  </div>
+                  <div className="modal fade " id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div className="modal-dialog">
+                      <div className="modal-content bg-dark">
+                        <div className="modal-header">
+                          <h2 className="modal-title fs-5" id="exampleModalLabel">{benefits.a}</h2>
+                          <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                        A dangling pointer in C is a pointer that points to a memory location that has been deallocated or is no longer valid. Dangling pointers can cause various problems in a program, including segmentation faults, memory leaks, and unpredictable behavior.
+                        </div>
+                        <div className="modal-footer">
+                          <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  {/* ----------------------------------------------------------------------- */}
                 </div>
               </div>
             </div>
